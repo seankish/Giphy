cad138ba58fc6a8d9ac21aaa4ac4363fb9a13ff7
skish1987 <seankish1@gmail.com>
2019-06-17 13:09:12 -0700

    buttons done

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,82 @@
-java
+$(document).ready(function() {
+    var topics = ["Hamburger", "hotdog", "Tacos", "Pizza", "soup","noodles","cookie","ice cream"]
+    GenerateButtons();
+    //  create buttons in html from array of strings
+    function GenerateButtons () {
+        $(".gif-buttons").empty();
+        // use loop to append buttons
+        for (var i=0; i < topics.length; i++) {
+            var button =$('<button>');
+            button.addClass("btn btn-warning food");
+            button.attr("data-name", topics[i]);
+            button.text(topics[i]);
+            $(".gif-buttons").append(button);
+        }
+    }   
+        // add new buttons to list
+        $(".submit").on("click", function(event) {
+        event.preventDefault();
+        var addFood = $("#user-input").val()
+        $("#user-input").val(" ")
+        topics.unshift(addFood);
+        GenerateButtons();
+        console.log("clicked")
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
