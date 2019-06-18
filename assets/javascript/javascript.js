63c9d694a9f71da4dce04c738525e9213b669747
skish1987 <seankish1@gmail.com>
2019-06-17 18:53:42 -0700

    gif display code in

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var topics = ["Hamburger", "hotdog", "Tacos", "Pizza", "soup","noodles","cookie","ice cream"]
+    var topics = ["Hamburger", "Hotdog", "Tacos", "Pizza", "Soup","Noodles","Cookie","Ice cream"]
     GenerateButtons();
     //  create buttons in html from array of strings
     function GenerateButtons () {
@@ -16,35 +16,49 @@ $(document).ready(function() {
         // add new buttons to list
         $(".submit").on("click", function(event) {
         event.preventDefault();
-        var addFood = $("#user-input").val()
-        $("#user-input").val(" ")
-        topics.unshift(addFood);
+        var addButton= $("#user-input").val()
+        topics.unshift(addButton);
         GenerateButtons();
-        console.log("clicked")
+
         })
 
+              // Use API to search for selected topic and display images
+            var food = $(this).attr("data-name");
+            var queryURL = $.get("http://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=qF26EQ71WEAdIYhWDmXlY3fLjbTDBcFC")
+            queryURL.done(function(data) { console.log("success got data", data); });
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            $(".gif-view").empty();
+            for (var i = 0; i<response.data.length; i++) { 
+                var topicDiv = $('<div class="topic">'); 
+                var image = response.data[i].images.fixed_height_still.url;
+                var rating = response.data[i].rating;
+                topicDiv.append(
+                    '<figure class="floatLeft">' + 
+                        '<img class="gif" src="' + image + '">' +
+                        '<figcaption>' + 'Rating: ' + rating + '</figcaption>' +
+                    '</figure>' 
+                );
 
+                // Display gifs on page
+                $(".gif-view").append(topicDiv);
+            }
+             // Display gifs of when selected topic is clicked
+    $(document).on("click", ".food", displayGifs);
 
+    generateButtons();
 
+        });
 
+  
 
+            
+            
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            
+        
 
 
 
