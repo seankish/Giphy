125e2542fe3ba0557748b5ac95bfe349a903ec6c
skish1987 <seankish1@gmail.com>
2019-06-18 10:31:27 -0700

    still images appending with ratings

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     var topics = ["Hamburger", "Hotdog", "Tacos", "Pizza", "Soup","Noodles","Cookie","Ice cream"]
-    GenerateButtons();
+    // GenerateButtons();
     //  create buttons in html from array of strings
     function GenerateButtons () {
         $(".gif-buttons").empty();
         // use loop to append buttons
         for (var i=0; i < topics.length; i++) {
-            var button =$('<button>');
+            var button =$('<button type="button">');
             button.addClass("btn btn-warning food");
             button.attr("data-name", topics[i]);
             button.text(topics[i]);
@@ -16,20 +16,24 @@ $(document).ready(function() {
         // add new buttons to list
         $(".submit").on("click", function(event) {
         event.preventDefault();
-        var addButton= $("#user-input").val()
-        topics.unshift(addButton);
+        var food= $('#user-input').val()
+        $("#user-input").text("hello")
+        topics.unshift(food);
         GenerateButtons();
+        console.log("clicked")
 
         })
 
               // Use API to search for selected topic and display images
+        function displayGifs() {
             var food = $(this).attr("data-name");
-            var queryURL = $.get("http://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=qF26EQ71WEAdIYhWDmXlY3fLjbTDBcFC")
-            queryURL.done(function(data) { console.log("success got data", data); });
+            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + food  + "&api_key=qF26EQ71WEAdIYhWDmXlY3fLjbTDBcFC"
+            // queryURL.done(function(data) { console.log("success got data", data); });
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
+            console.log(response)
             $(".gif-view").empty();
             for (var i = 0; i<response.data.length; i++) { 
                 var topicDiv = $('<div class="topic">'); 
@@ -37,7 +41,7 @@ $(document).ready(function() {
                 var rating = response.data[i].rating;
                 topicDiv.append(
                     '<figure class="floatLeft">' + 
-                        '<img class="gif" src="' + image + '">' +
+                '<img class="gif" src="' + image + '">' +
                         '<figcaption>' + 'Rating: ' + rating + '</figcaption>' +
                     '</figure>' 
                 );
@@ -45,19 +49,20 @@ $(document).ready(function() {
                 // Display gifs on page
                 $(".gif-view").append(topicDiv);
             }
-             // Display gifs of when selected topic is clicked
-    $(document).on("click", ".food", displayGifs);
-
-    generateButtons();
-
+            
+    
         });
 
-  
+    }
 
             
             
 
-            
+               
+    $(document).on("click", ".food", displayGifs);
+
+    GenerateButtons();
+      
         
 
 
